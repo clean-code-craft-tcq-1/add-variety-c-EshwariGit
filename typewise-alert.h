ebbefb2ae4dd4531a44a1b78b480e6a3b5d66329
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-03-30 23:17:59 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -17,8 +17,6 @@ typedef struct {
 	double upperLimit;
 }BreachLimit;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
