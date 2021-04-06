54c183734f4b4507077b2dedf619f7a766e2821f
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-04-06 22:48:27 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -12,12 +12,12 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 }
 
 BreachLimit classifyTemperatureBreach[3] = {{0,35}, {0,45}, {0,40}};
+
 InfoType(*Alert[])(BreachType) = {sendToController, sendToEmail,sendToConsole};
-InfoType checkAndAlert(
-    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+InfoType checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
   InfoType Value = FAIL;
   BreachType breachType = inferBreach(classifyTemperatureBreach[batteryChar.coolingType].lowerLimit,  classifyTemperatureBreach[batteryChar.coolingType].upperLimit, temperatureInC);
- Value = Alert[alertTarget](breachType);
+  Value = Alert[alertTarget](breachType);
   return Value;
 
 }
