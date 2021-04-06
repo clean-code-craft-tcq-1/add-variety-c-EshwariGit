a961a03814334c6c900bf120ef0a43370f7acc1d
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-04-06 22:56:53 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -15,8 +15,24 @@ TEST_CASE("infers the breach according which is too normal") {
   REQUIRE(inferBreach(30, 20, 30) == NORMAL);
 }
 
-TEST_CASE("Infers If Email Sending Working") {
+TEST_CASE("Infers If Email Sending Working -CASE1") {
   BatteryCharacter batterychar = {HI_ACTIVE_COOLING, "NORMAL"};
   REQUIRE(checkAndAlert(TO_EMAIL, batterychar,30 ) == PASS);
 }
+
+
+TEST_CASE("Infers If Email Sending Working -CASE2") {
+  BatteryCharacter batterychar = {HI_ACTIVE_COOLING, "HIGH"};
+  REQUIRE(checkAndAlert(TO_EMAIL, batterychar, 60 ) == PASS);
+}
+
+TEST_CASE("Infers If Email Sending Working -CASE3") {
+  BatteryCharacter batterychar = {HI_ACTIVE_COOLING, "LOW"};
+  REQUIRE(checkAndAlert(TO_EMAIL, batterychar, -4 ) == PASS);
+}
+TEST_CASE("Infers If Sending to Console is Working -CASE1") {
+  BatteryCharacter batterychar = {MED_ACTIVE_COOLING, "CONSOLE"};
+  REQUIRE(checkAndAlert(TO_CONSOLE, batterychar, 30 ) == PASS);
+}
+
 
